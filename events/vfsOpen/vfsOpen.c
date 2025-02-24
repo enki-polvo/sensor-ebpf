e4cc3b942ee239a7562e0a860a671aac0a68e351
knightchaser <agerio100@naver.com>
2025-02-24 19:12:14 +0900

    Dev: Show repeatedly traversals at dentry->d_parent at kprobe:vfs_open tracing

--- events/vfsOpen/vfsOpen.c
+++ events/vfsOpen/vfsOpen.c
@@ -22,37 +22,55 @@ struct {
 
 // Attach to vfs_open.
 // Prototype: int vfs_open(const struct path *path, struct file *file)
-// We extract the dentry from path, then get the file name from
-// dentry->d_name.name.
+// We extract the dentry from path, then traverse its d_parent chain.
 SEC("kprobe/vfs_open")
 int BPF_KPROBE(vfs_open, const struct path *path, struct file *file) {
-    struct file_create_event *e;
-    const struct dentry *dentry = 0;
+    struct dentry *dentry = 0;
     const unsigned char *fname_ptr = 0;
+    int i;
 
-    e = bpf_ringbuf_reserve(&vfsOpenEventMap, sizeof(*e), 0);
-    if (!e)
-        return 0;
-
-    e->pid = bpf_get_current_pid_tgid() >> 32;
-    e->uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-
-    // Read the dentry pointer from path->dentry.
+    // Read the initial dentry pointer from path->dentry.
     if (bpf_probe_read_kernel(&dentry, sizeof(dentry), &path->dentry) != 0) {
-        bpf_ringbuf_discard(e, 0);
         return 0;
     }
 
-    // Read the filename pointer from dentry->d_name.name.
-    if (bpf_probe_read_kernel(&fname_ptr, sizeof(fname_ptr),
-                              &dentry->d_name.name) != 0) {
-        bpf_ringbuf_discard(e, 0);
-        return 0;
+    // Traverse up to 50 levels of parent dentries.
+    // TODO: Later, we'll concatenate each filename into a full path.
+    for (i = 0; i < 50 && dentry != NULL; i++) {
+        struct file_create_event *e;
+
+        // Reserve a new event for the current dentry.
+        e = bpf_ringbuf_reserve(&vfsOpenEventMap, sizeof(*e), 0);
+        if (!e)
+            return 0;
+
+        e->pid = bpf_get_current_pid_tgid() >> 32;
+        e->uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+
+        // Read the filename pointer from dentry->d_name.name.
+        if (bpf_probe_read_kernel(&fname_ptr, sizeof(fname_ptr),
+                                  &dentry->d_name.name) != 0) {
+            bpf_ringbuf_discard(e, 0);
+            break;
+        }
+
+        // Copy the filename string into our event buffer.
+        bpf_probe_read_kernel_str(e->filename, sizeof(e->filename), fname_ptr);
+
+        // Discard if e->filename is "/"
+        if (e->filename[0] == '/' && e->filename[1] == '\0') {
+            bpf_ringbuf_discard(e, 0);
+            break;
+        }
+
+        bpf_ringbuf_submit(e, 0);
+
+        // Move to the parent dentry.
+        if (bpf_probe_read_kernel(&dentry, sizeof(dentry), &dentry->d_parent) !=
+            0) {
+            break;
+        }
     }
 
-    // Copy the filename from the pointer into our event buffer.
-    bpf_probe_read_kernel_str(e->filename, sizeof(e->filename), fname_ptr);
-
-    bpf_ringbuf_submit(e, 0);
     return 0;
 }
